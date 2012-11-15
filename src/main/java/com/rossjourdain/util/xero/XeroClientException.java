e389847f608ae57781cdd6e3d5cbaaea17974ac9
Ross Jourdain <ross.jourdain@abletech.co.nz>
2012-11-15 20:34:25 +1300

    Refactor client exception

--- src/main/java/com/rossjourdain/util/xero/XeroClientException.java
+++ src/main/java/com/rossjourdain/util/xero/XeroClientException.java
@@ -33,6 +33,9 @@ import org.w3c.dom.Element;
 public class XeroClientException extends Exception {
 
   private ApiExceptionExtended apiException;
+  private List<ValidationError> validationErrors;
+  private List<Warning> warnings;
+  private Object modelObject;
 
   public XeroClientException(String message, OAuthProblemException oAuthProblemException) {
 
@@ -43,77 +46,121 @@ public class XeroClientException extends Exception {
     oAuthProblemExceptionString = XeroXmlManager.oAuthProblemExceptionToXml(oAuthProblemException);
     System.out.println(oAuthProblemExceptionString);
     apiException = (ApiExceptionExtended) XeroXmlManager.xmlToException(oAuthProblemExceptionString);
+    unmarshalAdditionalData();
+
+    /* Add this back in if you need more details on the exception */
+    //System.out.println("" + apiExceptionString);
+    //System.out.println("");
   }
 
   public ApiException getApiException() {
     return apiException;
   }
 
-  public void printDetails() {
-    
+  public List<ValidationError> getValidationErrors() {
+    return validationErrors;
+  }
+
+  public List<Warning> getWarnings() {
+    return warnings;
+  }
+
+  public Object getModelObject() {
+    return modelObject;
+  }
+
+  private void unmarshalAdditionalData() {
+
     try {
-      
-      System.out.println("");
-      System.out.println(this.getMessage());
-      System.out.println("Message: " + apiException.getMessage());
-      System.out.println("Error " + apiException.getErrorNumber() + ": " + apiException.getType());
 
       Element e = (Element) apiException.getElements().getDataContractBase();
       String elementType = e.getAttribute("xsi:type");
 
       JAXBContext context = JAXBContext.newInstance(ResponseType.class);
       Unmarshaller unmarshaller = context.createUnmarshaller();
+
       // unmarshaller.setEventHandler(new DefaultValidationEventHandler());
 
       JAXBElement jaxbElement = null;
-
       System.out.println("Type is: " + elementType);
 
-
       if ("Invoice".equals(elementType)) {
-        System.out.println("Processing Invoice");
         jaxbElement = unmarshaller.unmarshal(new DOMStreamReader(e), Invoice.class);
-        Invoice invoice = (Invoice) jaxbElement.getValue();
-        System.out.println("Invoice ID: " + invoice.getInvoiceID());
-        if (invoice.getDate() != null) {
-          System.out.println("Invoice Date  : " + invoice.getDate().getTime());
-        }
+        modelObject = (Invoice) jaxbElement.getValue();
       } else if ("Payment".equals(elementType)) {
-        System.out.println("Processing Payment");
         jaxbElement = unmarshaller.unmarshal(new DOMStreamReader(e), Payment.class);
-        Payment payment = (Payment) jaxbElement.getValue();
-        System.out.println("Payment ID  : " + payment.getPaymentID());
-        if (payment.getDate() != null) {
-          System.out.println("Payment Date  : " + payment.getDate().getTime());
-        }
+        modelObject = (Payment) jaxbElement.getValue();
+      } else if ("Contact".equals(elementType)) {
+        jaxbElement = unmarshaller.unmarshal(new DOMStreamReader(e), Contact.class);
+        modelObject = (Contact) jaxbElement.getValue();
       } else {
         throw new RuntimeException("Unrecognised type: " + elementType);
       }
 
       if (jaxbElement != null) {
         DataContractBase dataContractBase = (DataContractBase) jaxbElement.getValue();
+
         if (dataContractBase.getWarnings() != null && dataContractBase.getWarnings().getWarning() != null) {
-          List<Warning> warnings = dataContractBase.getWarnings().getWarning();
-          for (int i = 0; i < warnings.size(); i++) {
-            Warning warning = warnings.get(i);
-            System.out.println("Warning " + (i + 1) + ": " + warning.getMessage());
-          }
+          warnings = dataContractBase.getWarnings().getWarning();
         }
         if (dataContractBase.getValidationErrors() != null && dataContractBase.getValidationErrors().getValidationError() != null) {
-          List<ValidationError> validationErrors = dataContractBase.getValidationErrors().getValidationError();
-          for (int i = 0; i < validationErrors.size(); i++) {
-            ValidationError validationError = validationErrors.get(i);
-            System.out.println("Validation Error " + (i + 1) + ": " + validationError.getMessage());
-          }
+          validationErrors = dataContractBase.getValidationErrors().getValidationError();
         }
       }
 
-      System.out.println("");
-      /* Add this back in if you need more details on the exception */
-      //System.out.println("" + apiExceptionString);
-      //System.out.println("");
     } catch (JAXBException ex) {
       ex.printStackTrace();
     }
+
+  }
+
+  public void printDetails() {
+
+    System.out.println("");
+    System.out.println(this.getMessage());
+    System.out.println("Message: " + apiException.getMessage());
+    System.out.println("Error " + apiException.getErrorNumber() + ": " + apiException.getType());
+    
+    System.out.println("" + modelObject);
+    System.out.println("" + modelObject.getClass());
+    
+    
+    if (modelObject instanceof Invoice) {
+        System.out.println("Processing Invoice");
+        Invoice invoice = (Invoice) modelObject;
+
+        System.out.println("Invoice ID: " + invoice.getInvoiceID());
+        if (invoice.getDate() != null) {
+          System.out.println("Invoice Date: " + invoice.getDate().getTime());
+        }
+      } else if (modelObject instanceof Payment) {
+        System.out.println("Processing Payment");
+        Payment payment = (Payment) modelObject;
+
+        System.out.println("Payment ID: " + payment.getPaymentID());
+        if (payment.getDate() != null) {
+          System.out.println("Payment Date: " + payment.getDate().getTime());
+        }
+      } else if (modelObject instanceof Contact) {
+        System.out.println("Processing Contact");
+        Contact contact = (Contact) modelObject;
+
+        System.out.println("Contact ID: " + contact.getContactID());
+        System.out.println("Contact Name: " + contact.getName());
+      } else {
+        System.out.println("Unrecognised type: " + modelObject);
+      }
+    
+
+    for (int i = 0; i < warnings.size(); i++) {
+      Warning warning = warnings.get(i);
+      System.out.println("Warning " + (i + 1) + ": " + warning.getMessage());
+    }
+
+    for (int i = 0; i < validationErrors.size(); i++) {
+      ValidationError validationError = validationErrors.get(i);
+      System.out.println("Validation Error " + (i + 1) + ": " + validationError.getMessage());
+    }
+
   }
 }
